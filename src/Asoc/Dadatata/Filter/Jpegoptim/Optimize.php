9ca5399392985b55a92d8be5b94ef90bfce6bc80
Pascal Kuendig <padakuro@gmail.com>
2014-02-27 14:16:21 +0100

    implement jpegoptim filter

---
+++ src/Asoc/Dadatata/Filter/Jpegoptim/Optimize.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Asoc\Dadatata\Filter\Jpegoptim;
+
+use Asoc\Dadatata\Exception\ProcessingFailedException;
+use Asoc\Dadatata\Filter\FilterInterface;
+use Asoc\Dadatata\Filter\OptionsInterface;
+use Asoc\Dadatata\Model\ImageInterface;
+use Asoc\Dadatata\Model\ThingInterface;
+use Symfony\Component\Process\ProcessBuilder;
+
+class Optimize implements FilterInterface {
+
+    /**
+     * @var
+     */
+    private $bin;
+    /**
+     * @var OptimizeOptions
+     */
+    private $defaults;
+
+    public function __construct($bin) {
+        $this->bin = $bin;
+    }
+
+    /**
+     * @param OptionsInterface $options
+     */
+    public function setOptions(OptionsInterface $options)
+    {
+        if(!($options instanceof OptimizeOptions)) {
+            $options = new OptimizeOptions($options->all());
+        }
+        $this->defaults = $options;
+    }
+
+    /**
+     * @param ThingInterface $thing
+     * @param string $sourcePath
+     * @param \Asoc\Dadatata\Filter\OptionsInterface|null $options
+     * @return array Paths to generated files
+     */
+    public function process(ThingInterface $thing, $sourcePath, OptionsInterface $options = null)
+    {
+        // jpegoptim wants a directory
+        $tmpPath = sys_get_temp_dir();
+
+        /** @var OptimizeOptions $options */
+        $options = $this->defaults->merge($options);
+
+        $pb = new ProcessBuilder([$this->bin]);
+        $pb->add('--dest')->add($tmpPath);
+        $pb->add('--max')->add($options->getQuality());
+        $pb->add('--strip-'.$options->getStrip());
+
+        if($options->has(OptimizeOptions::OPTION_THRESHOLD)) {
+            $pb->add('--threshold')->add($options->getThreshold());
+        }
+
+        $pb->add($sourcePath);
+        $process = $pb->getProcess();
+
+        $code = $process->run();
+        if($code !== 0) {
+            throw ProcessingFailedException::create('Failed to optimize JPEG', $code, $process->getOutput(), $process->getErrorOutput());
+        }
+
+        // output directory + name of file
+        return [sprintf('%s/%s', $tmpPath, basename($sourcePath))];
+    }
+
+    /**
+     * @param ThingInterface $thing
+     * @return boolean
+     */
+    public function canHandle(ThingInterface $thing)
+    {
+        return $thing instanceof ImageInterface;
+    }
+}
---
+++ src/Asoc/Dadatata/Filter/Jpegoptim/OptimizeOptions.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Asoc\Dadatata\Filter\Jpegoptim;
+
+use Asoc\Dadatata\Filter\Options;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class OptimizeOptions extends Options {
+
+    const OPTION_QUALITY = 'quality';
+    const OPTION_THRESHOLD = 'threshold';
+    const OPTION_ALL = 'all';
+    const OPTION_STRIP = 'all';
+
+    public function getQuality() {
+        return $this->options[self::OPTION_QUALITY];
+    }
+
+    public function getThreshold() {
+        return $this->options[self::OPTION_THRESHOLD];
+    }
+
+    public function getAll() {
+        return $this->options[self::OPTION_ALL];
+    }
+
+    public function getStrip() {
+        return $this->options[self::OPTION_STRIP];
+    }
+
+    protected function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults([
+            self::OPTION_STRIP => 'all'
+        ]);
+        $resolver->setRequired([
+            self::OPTION_QUALITY
+        ]);
+        $resolver->setOptional([
+            self::OPTION_THRESHOLD,
+            self::OPTION_ALL,
+            self::OPTION_STRIP
+        ]);
+        $resolver->setAllowedValues([
+            self::OPTION_ALL => ['normal', 'progressive'],
+            self::OPTION_STRIP => ['all', 'com', 'exif', 'iptc', 'icc']
+        ]);
+    }
+
+} 
