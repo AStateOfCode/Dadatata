99f0aad388a405c96fc00addf17c143ce4d215a4
Pascal Kuendig <padakuro@gmail.com>
2014-02-28 08:37:08 +0100

    add mapping for archive types

--- src/Asoc/Dadatata/Metadata/MimeTypeGuesser.php
+++ src/Asoc/Dadatata/Metadata/MimeTypeGuesser.php
@@ -29,6 +29,9 @@ class MimeTypeGuesser implements TypeGuesserInterface {
             case isset(static::$mimeCategoryMapText[$mime]):
                 return self::CATEGORY_TEXT;
 
+            case isset(static::$mimeCategoryMapArchive[$mime]):
+                return self::CATEGORY_ARCHIVE;
+
             case isset(static::$mimeCategoryMapDocument[$mime]):
                 return self::CATEGORY_DOCUMENT;
         }
@@ -49,6 +52,39 @@ class MimeTypeGuesser implements TypeGuesserInterface {
 
         return null;
     }
+
+    // http://en.wikipedia.org/wiki/List_of_archive_formats
+    public static $mimeCategoryMapArchive = [
+        'application/x-cpio' => true,
+        'application/x-shar' => true,
+        'application/x-tar' => true,
+        'application/x-bzip2' => true,
+        'application/x-gzip' => true,
+        'application/x-lzip' => true,
+        'application/x-lzma' => true,
+        'application/x-lzop' => true,
+        'application/x-xz' => true,
+        'application/x-compress' => true,
+        'application/x-7z-compressed' => true,
+        'application/x-ace-compressed' => true,
+        'application/x-alz-compressed' => true,
+        'application/vnd.android.package-archive' => true,
+        'application/x-arj' => true,
+        'application/vnd.ms-cab-compressed' => true,
+        'application/x-cfs-compressed' => true,
+        'application/x-dar' => true,
+        'application/x-dgc-compressed' => true,
+        'application/x-apple-diskimage' => true,
+        'application/x-gca-compressed' => true,
+        'application/x-lzh' => true,
+        'application/x-lzx' => true,
+        'application/x-rar-compressed' => true,
+        'application/x-stuffit' => true,
+        'application/x-stuffitx' => true,
+        'application/x-gtar' => true,
+        'application/zip' => true,
+        'application/x-zoo' => true
+    ];
 
     public static $mimeCategoryMapImage = [
         'application/postscript' => true,
@@ -88,6 +124,7 @@ class MimeTypeGuesser implements TypeGuesserInterface {
         'application/vnd.ms-powerpoint' => true,
         'application/vnd.ms-excel' => true,
 
+        // http://technet.microsoft.com/en-us/library/ee309278%28office.12%29.aspx
         // microsoft office >=2007
         'application/vnd.openxmlformats-officedocument.wordprocessingml.document' => true,
         'application/vnd.openxmlformats-officedocument.wordprocessingml.template' => true,
